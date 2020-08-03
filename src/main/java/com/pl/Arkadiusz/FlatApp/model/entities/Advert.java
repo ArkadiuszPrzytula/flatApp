ac4f263b6d5b1299596caacb43c7ce04c4686e74
aspeedi777 <aspeedi777@gmail.com>
2020-08-03 17:45:18 +0200

    Entities: entity base and advert

---
+++ src/main/java/com/pl/Arkadiusz/FlatApp/model/entities/Advert.java
@@ -0,0 +1,31 @@
+package com.pl.Arkadiusz.FlatApp.model.entities;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+
+@Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @ToString(exclude = {"user"}) @EqualsAndHashCode (of = "id")
+    @Builder
+    @Entity
+    @Table(name = "adverts")
+    public class Advert extends EntityBase{
+
+        @Column(nullable = false)
+        private String title;
+        @Column(nullable = false)
+        private String description;
+        @Column(nullable = false)
+        private Boolean active;
+
+        @ManyToOne
+        @JoinColumn(name="user_id", nullable = false)
+        private User user;
+
+    }
+
